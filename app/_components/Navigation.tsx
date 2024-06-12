cb567f3ee87ee4f74eba08da5ba602fd6b3812fa
Olatoyan <golatoyan@gmail.com>
2024-06-12 08:33:41 +0700

    animated the navigation component

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -5,14 +5,35 @@ import Image from "next/image";
 import Logo from "@/public/logo-dark.png";
 import { useState } from "react";
 import useMediaQuery from "../_hooks/useMediaQuery";
+import { AnimatePresence, motion } from "framer-motion";
+
+const navVariants = {
+  hidden: { opacity: 0, y: -20, scale: 0.9 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+      when: "beforeChildren",
+      staggerChildren: 0.1,
+    },
+  },
+  exit: { opacity: 0, y: -20, scale: 0.9, transition: { duration: 0.5 } },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 10 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+  exit: { opacity: 0, y: 10, transition: { duration: 0.3 } },
+};
 
 function Navigation() {
   const [isNavOpen, setIsNavOpen] = useState(false);
-
   const isTablet = useMediaQuery("(max-width: 700px)");
 
   return (
-    <header className="tablet:px-10 tablet:py-14 tablet:relative z-[11] px-[14rem] py-[6.4rem] desktop:px-24">
+    <header className="z-[11] px-[14rem] py-[6.4rem] desktop:px-24 tablet:relative tablet:px-10 tablet:py-14">
       <nav className="flex items-center justify-between">
         <Link href="/">
           <Image
@@ -24,22 +45,42 @@ function Navigation() {
           />
         </Link>
 
-        {(!isTablet || (isTablet && isNavOpen)) && (
-          <ul className="tablet:absolute tablet:flex-col tablet:top-[9.6rem] tablet:left-0 tablet:w-full tablet:items-start tablet:bg-[#1d1c1e] tablet:z-10 tablet:py-20 tablet:px-10 flex items-center gap-[4.2rem]">
-            <li className="tablet:text-white tablet:text-[1.8rem] text-[1.4rem] uppercase leading-[100%] tracking-[0.2rem] text-[#333136] transition-all duration-300 hover:underline">
-              <Link href="/about">Our company</Link>
-            </li>
-            <li className="tablet:text-white tablet:text-[1.8rem] text-[1.4rem] uppercase leading-[100%] tracking-[0.2rem] text-[#333136] transition-all duration-300 hover:underline">
-              <Link href="/location">Locations</Link>
-            </li>
-            <li className="tablet:text-white tablet:text-[1.8rem] text-[1.4rem] uppercase leading-[100%] tracking-[0.2rem] text-[#333136] transition-all duration-300 hover:underline">
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
-        )}
+        <AnimatePresence>
+          {(!isTablet || (isTablet && isNavOpen)) && (
+            <motion.ul
+              className="flex items-center gap-[4.2rem] tablet:absolute tablet:left-0 tablet:top-[9.6rem] tablet:z-10 tablet:w-full tablet:flex-col tablet:items-start tablet:bg-[#1d1c1e] tablet:px-10 tablet:py-20"
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={navVariants}
+            >
+              <motion.li
+                className="text-[1.4rem] uppercase leading-[100%] tracking-[0.2rem] text-[#333136] transition-all duration-300 hover:underline tablet:text-[1.8rem] tablet:text-white"
+                variants={itemVariants}
+                onClick={() => setIsNavOpen(false)}
+              >
+                <Link href="/about">Our company</Link>
+              </motion.li>
+              <motion.li
+                className="text-[1.4rem] uppercase leading-[100%] tracking-[0.2rem] text-[#333136] transition-all duration-300 hover:underline tablet:text-[1.8rem] tablet:text-white"
+                variants={itemVariants}
+                onClick={() => setIsNavOpen(false)}
+              >
+                <Link href="/location">Locations</Link>
+              </motion.li>
+              <motion.li
+                className="text-[1.4rem] uppercase leading-[100%] tracking-[0.2rem] text-[#333136] transition-all duration-300 hover:underline tablet:text-[1.8rem] tablet:text-white"
+                variants={itemVariants}
+                onClick={() => setIsNavOpen(false)}
+              >
+                <Link href="/contact">Contact</Link>
+              </motion.li>
+            </motion.ul>
+          )}
+        </AnimatePresence>
 
         <button
-          className="tablet:block hidden"
+          className="hidden tablet:block"
           onClick={() => setIsNavOpen((prev) => !prev)}
         >
           <Image
@@ -57,7 +98,6 @@ function Navigation() {
             className={`${isNavOpen ? "block" : "hidden"}`}
           />
         </button>
-        {/* <div className="fixed left-0 top-0 z-[9] min-h-[100dvh] w-full bg-black/50"></div> */}
       </nav>
     </header>
   );
